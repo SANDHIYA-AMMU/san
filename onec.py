685f13550260cb462eec95a5d1ba539c6987403d
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2017-11-13 18:36:33 +0530

    Update onec.py

--- onec.py
+++ onec.py
@@ -1,4 +1,6 @@
-l=list(map(int,input().split(' ')))
-for i in l:
-    if(l.count(i)==1):
-        print(i)
+l1=list(map(int,input().split(' ')))
+for i in l1:
+    if(l1.count(i)==1):
+        print(i)    
+        
+        
