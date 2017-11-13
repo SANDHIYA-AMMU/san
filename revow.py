21a0cc96246c303121e51ad7ce20af4b06a83c08
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2017-11-13 18:30:41 +0530

    Update revow.py

--- revow.py
+++ revow.py
@@ -1,7 +1,7 @@
-v=['a','e','i','o','u','A','E','I','O','U']
+vow=['a','e','i','o','u','A','E','I','O','U']
 n=list(input())[::-1]
 k=[]
 for i in n:
-    if i not in v:
+    if i not in vow:
         k.append(i)
 print("".join(str(x) for x in (k)))
