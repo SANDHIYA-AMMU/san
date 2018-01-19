f371649a9736468753c5c5195b8f186786e70872
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2018-01-19 12:05:00 +0530

    Create 50.py

---
+++ 50.py
@@ -0,0 +1,14 @@
+#50 th prb
+def isPowerOfTwo(n):
+    if (n == 0):
+        return False
+    while (n != 1):
+            if (n % 2 != 0):
+                return False
+            n = n // 2
+    return True
+n=int(input())
+if(isPowerOfTwo(n)):
+    print('Yes')
+else:
+    print('No')
