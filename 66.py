3cca9f43b19b633c45563a045644a8455c82fb05
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2018-01-19 10:41:44 +0530

    Create 66.py

---
+++ 66.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n > 1:
+   for i in range(2,n):
+       if (n % i) == 0:
+           print("No")
+           break
+       else:
+           print("Yes")
+
+else:
+   print("No")
