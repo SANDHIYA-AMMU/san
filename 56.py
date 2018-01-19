a7398a1d8116947813d52f85b56699ec7c6a9145
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2018-01-19 12:21:18 +0530

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,14 @@
+#56th
+n=input()
+c=0
+d=0
+for char in n:
+    if(char.isdigit()):
+        c=c+1
+    if(char.isalpha()):
+        d=d+1
+    if(c>0 and d>0):
+        print('yes')
+        break
+if(c==0 or d==0):
+        print('No')
