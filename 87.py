f7a16e86cea30026f1b7bea5950e59e56fb0c9d5
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2018-01-20 13:01:06 +0530

    Create 87.py

---
+++ 87.py
@@ -0,0 +1,10 @@
+#87th prb
+x,y=map(int,input().split(' '))
+if x > y:
+    smaller = y
+else:
+    smaller = x
+for i in range(1, smaller+1):
+    if((x % i == 0) and (y % i == 0)):
+        gcd = i
+print(gcd)
