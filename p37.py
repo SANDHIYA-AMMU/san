c8d51c191ce97da0bbb222d6e1e9e95cf9ec72e9
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2018-02-02 13:41:55 +0530

    Update p37.py

--- p37.py
+++ p37.py
@@ -10,10 +10,5 @@ for x in p1:
 			p2.append(y)
 print(min(p2))
 
-#38 problem
-v=int(input())
-l=[]
-for i in range(2,v+1,2):
-    if v%i==0:
-        l.append(i)
-print(" ".join (str(i) for i in l))
+
+
