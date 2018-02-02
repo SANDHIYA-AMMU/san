ca4d8247fb22075c060e6bae9b5c51ee83cad549
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2018-02-02 13:28:55 +0530

    Create p21.py

---
+++ p21.py
@@ -0,0 +1,5 @@
+a1,b1=map(int,input("Enter 1st line").split(' '))
+a2,b2=map(int,input("Enter 2nd line").split(' '))
+a3,b3=map(int,input("Enter 3rd line").split(' '))
+if (a1==b2 and a2==a3) or(b1==b2 and b2==b3) or (abs(a1-a2)==abs(a2-a3) and abs(b1-b2)==abs(b2-b3)):print("yes")
+else:print("no")
