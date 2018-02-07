54e2099ef25c6b42322179be85889fa52a080093
SANDHIYA-AMMU <33615134+SANDHIYA-AMMU@users.noreply.github.com>
2018-02-07 15:40:19 +0530

    Create p45.py

---
+++ p45.py
@@ -0,0 +1,9 @@
+#45th prb
+p=int(input().split(' '))
+s=int(p[1]**0.5)
+if float(s)==float(p[1]**0.5):
+    if int(p[1]**(0.5))==(p[0]//4):
+        print('Yes')
+    else:
+        print('No')
+else: print('No')
